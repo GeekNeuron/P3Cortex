a19eaa6b20a1999adaa91d959ccb0d774b6a925d
Geek Neuron <geekneuron@gmail.com>
2025-07-10 00:38:22 +0330

    Update app.js

--- js/app.js
+++ js/app.js
@@ -226,14 +226,15 @@ const renderShowQuestionsButton = () => {
     const renderSavedQuestions = () => {
     savedQuestionsContainer.innerHTML = '';
     if (savedQuestions.length === 0) {
+    savedQuestionsContainer.innerHTML = `
         <p class="empty-message">
-                هنوز سوالی برای مرور نشان نکرده‌اید.<br>
-                از بخش <b>نمونه سوالات</b>، روی آیکون 
-                <img src="images/bookmark-add.svg" alt="نشان" style="width:18px; height:18px; vertical-align:middle; filter:invert(0.5);">
-                زیر هر سوال کلیک کنید تا به این بخش اضافه شود.
-            </p>`;
-        return;
-    }
+            هنوز سوالی برای مرور نشان نکرده‌اید.<br>
+            از بخش <b>نمونه سوالات</b>، روی آیکون 
+            <img src="images/bookmark-add.svg" alt="نشان" style="width:18px; height:18px; vertical-align:middle; filter:invert(0.5);">
+            زیر هر سوال کلیک کنید تا به این بخش اضافه شود.
+        </p>`;
+    return;
+}
     const fragment = document.createDocumentFragment();
     savedQuestions.forEach(savedItem => {
         const question = allSections[savedItem.sectionIndex]?.find(q => q.id === savedItem.questionId);
